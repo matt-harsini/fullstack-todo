d15d946c7e99ad7f802a03183e55307be0a7b3e4
Matthew <matthewkimcs@gmail.com>
2023-03-30 15:52:00 -0700

    established connection to database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 const PORT = 3000;
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database connected");
+});
 
 app.use(cors());
 
